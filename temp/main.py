e16158574b4009557c2fdc6b254ff0b8ef65ed01
Chen Peng <chen56@msn.com>
2025-12-27 17:15:38 +0800

    temp

--- temp/main.py
+++ temp/main.py
@@ -1,8 +1,27 @@
-def main():
-    print("Hello from temp!")
-    print(__file__)
-    print(dir())
+from typing import Annotated, Callable, Coroutine
+from fastapi.responses import HTMLResponse, RedirectResponse
+import marimo
+from fastapi import FastAPI, Form, Request, Response
 
-print(dir())
+
+# Create a marimo asgi app
+server = (
+    marimo.create_asgi_app()
+    .with_app(path="", root="./pages/index.py")
+    .with_app(path="/dashboard", root="./pages/dashboard.py")
+    .with_app(path="/sales", root="./pages/sales.py")
+)
+
+# Create a FastAPI app
+app = FastAPI()
+
+# app.add_middleware(auth_middleware)
+# app.add_route("/login", my_login_route, methods=["POST"])
+
+app.mount("/", server.build())
+
+# Run the server
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
---
+++ temp/pages/index.py
@@ -0,0 +1,22 @@
+import marimo
+
+__generated_with = "0.18.4"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    # index
+    """)
+    return
+
+
+if __name__ == "__main__":
+    app.run()
